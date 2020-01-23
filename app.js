ab8b5a8c25a864dd7fc6f11d92c120aeabcbc47a
Godwin Olletey <godwinolle@gmail.com>
2020-01-23 06:43:27 -0500

    Added the intro and also the full about me

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ const seeMore = document.querySelector('.seeMore');
 seeMore.addEventListener('click', ()=>{
     location.href = 'https://www.instagram.com/godwin.codes';
 });
+
+
+const text = ['a Student', 'a Learner', 'an aspiring Full-Stack Developer'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+function typeWriter(){
+    const about = document.querySelector('.text');
+    if(count < text.length){
+        about.innerHTML = text[index];
+        count++;
+        index++;
+        setTimeout(typeWriter, 2000);
+    }
+}
+
+typeWriter();
