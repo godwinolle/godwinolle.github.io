b11a2a421e904d73cb2635e030fdba1b687d5392
Godwin Olletey <godwinolle@gmail.com>
2020-01-23 04:34:17 -0500

    Added all functionality to the home page

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ hamburger.addEventListener('click', () =>{
     console.log("Hamburger clicked")
     mobileMenu.classList.toggle('togglerr');
 });
+
+const seeMore = document.querySelector('.seeMore');
+
+seeMore.addEventListener('click', ()=>{
+    location.href = 'https://www.instagram.com/godwin.codes';
+});
