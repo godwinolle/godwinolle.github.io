0cdba2591bf23fd85adca173923505b55f8896e9
Godwin Olletey <godwinolle@gmail.com>
2020-01-28 17:20:59 -0500

    Added footer links

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ hamburger.addEventListener('click', () =>{
     mobileMenu.classList.toggle('togglerr');
 });
 
+
 const seeMore = document.querySelector('.seeMore');
 
 seeMore.addEventListener('click', ()=>{
     location.href = 'https://www.instagram.com/godwin.codes';
+});
+
+
+const instagramFooter = document.querySelector('.instagram-footer');
+instagramFooter.addEventListener('click', ()=>{
+    open('https://www.instagram.com/godwin.codes', '_blank');
+});
+
+
+const githubFooter = document.querySelector('.github-footer');
+githubFooter.addEventListener('click', ()=>{
+    open('https://github.com/godwinolle', '_blank');
 });
 
 
