6fb283e29df2f4d3103456e715f3d679c20ebf6c
Godwin Olletey <godwinolle@gmail.com>
2020-03-17 23:18:39 -0400

    Added moveable header that changes background

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ nameHome.addEventListener('click', ()=>{
     location.href = 'index.html';
 })
 
+const headerFollow = document.querySelector('header');
+window.addEventListener('scroll', ()=>{
+    if(window.pageYOffset > 50){
+        headerFollow.classList.add('activity');
+    }
+    else{
+        headerFollow.classList.remove('activity');
+    }
+});
 
+//this is the tagline of the home page
 let i=0;
 let text='Establishing Clean Code Since 1999';
 const typing = () =>{
@@ -22,19 +32,19 @@ const typing = () =>{
 };
 typing();
 
-
+//this is a redirect to my instagram page
 const seeMore = document.querySelector('.seeMore');
 seeMore.addEventListener('click', ()=>{
     location.href = 'https://www.instagram.com/godwin.codes';
 });
 
-
+//a redirect to my instagram located in the footer
 const instagramFooter = document.querySelector('.instagram-footer');
 instagramFooter.addEventListener('click', ()=>{
     open('https://www.instagram.com/godwin.codes', '_blank');
 });
 
-
+//a redirect to my github located in the footer
 const githubFooter = document.querySelector('.github-footer');
 githubFooter.addEventListener('click', ()=>{
     open('https://github.com/godwinolle', '_blank');
